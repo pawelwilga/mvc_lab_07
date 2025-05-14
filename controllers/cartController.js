138dd60f6a25e2eea6c59cb20e838e71dc142bd9
pawelwilga <pawel@wilga..net.pl>
2025-05-14 07:12:12 +0200

    Migracja kontrolera i modelu Cart na mongoDB

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -3,13 +3,20 @@ const Cart = require("../models/Cart");
 
 const { STATUS_CODE } = require("../constants/statusCode");
 
-exports.addProductToCart = (request, response) => {
-  Product.add(request.body);
-  Cart.add(request.body.name);
-
-  response.status(STATUS_CODE.FOUND).redirect("/products/new");
+exports.addProductToCart = async (request, response) => {
+  try {
+    await Product.add(request.body);
+    await Cart.add(request.body.name);
+    response.status(STATUS_CODE.FOUND).redirect("/products/new");
+  } catch (error) {
+    response.status(STATUS_CODE.INTERNAL_SERVER_ERROR).json({ error: error.message });
+  }
 };
 
-exports.getProductsCount = () => {
-  return Cart.getProductsQuantity();
+exports.getProductsCount = async () => {
+  try {
+    return await Cart.getProductsQuantity();
+  } catch (error) {
+    return 0;
+  }
 };
